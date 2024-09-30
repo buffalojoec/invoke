50de25bbe07f7b052172753607bc5242108f1ec3
Joe Caulfield <joe.caulfield@anza.xyz>
2024-09-30 18:25:01 +0700

    serialize

--- invoke/src/serialize.rs
+++ invoke/src/serialize.rs
@@ -1,11 +1,35 @@
 //! Parameter serialization.
 
-use solana_rbpf::{aligned_memory::AlignedMemory, ebpf::HOST_ALIGN, memory_region::MemoryRegion};
+use {
+    serde::Serialize,
+    solana_rbpf::{
+        aligned_memory::AlignedMemory,
+        ebpf::{HOST_ALIGN, MM_INPUT_START},
+        memory_region::MemoryRegion,
+    },
+};
 
-pub trait InvokeSerialize {}
+pub trait InvokeSerialize {
+    fn serialize(&self) -> Vec<u8>;
+}
+
+impl<T> InvokeSerialize for T
+where
+    T: Serialize,
+{
+    fn serialize(&self) -> Vec<u8> {
+        bincode::serialize(self).expect("Failed to serialize.")
+    }
+}
 
 pub(crate) fn serialize_params<T: InvokeSerialize>(
-    _params: T,
+    params: T,
 ) -> (AlignedMemory<HOST_ALIGN>, Vec<MemoryRegion>) {
-    todo!()
+    let data = params.serialize();
+    let mut aligned_memory = AlignedMemory::from_slice(&data);
+    let regions = vec![MemoryRegion::new_writable(
+        aligned_memory.as_slice_mut(),
+        MM_INPUT_START,
+    )];
+    (aligned_memory, regions)
 }
